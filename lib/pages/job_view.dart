dc3d6fcbe1e1c50b86a46782d9c21b8ac0c8a458
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-01-18 23:01:44 +0530

    jobdetails edited

--- lib/pages/job_view.dart
+++ lib/pages/job_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-
 import 'package:job_portal_app/models/job.dart';
 import 'package:job_portal_app/widgets/apply_button.dart';
 import 'package:job_portal_app/widgets/job_details.dart';
@@ -19,42 +17,52 @@ class JobView extends StatelessWidget {
     required this.companyDescription,
     required this.companyEmail,
     required this.companyLogo,
-
   }) : super(key: key);
-
-  
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Container(
-          height: 900 * 0.85,
-          width: double.infinity,
-          padding: const EdgeInsets.symmetric(horizontal: 1.5 * 16.0),
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(50.0),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black12,
-                offset: Offset(3, 0),
-                blurRadius: 8.0,
-                spreadRadius: 5.0,
-              ),
-            ],
-          ),
-          child: JobDetails(job: job,
-          companyDescription: companyDescription,
-          companyName: companyName,
-          companyEmail: companyEmail,
-          companyLogo: companyLogo,),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Job Details'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
-        const ApplyButton(),
-      ],
+      ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            height: 900 * 0.85,
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 1.5 * 16.0),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(50.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(3, 0),
+                  blurRadius: 8.0,
+                  spreadRadius: 5.0,
+                ),
+              ],
+            ),
+            child: JobDetails(
+              job: job,
+              companyDescription: companyDescription,
+              companyName: companyName,
+              companyEmail: companyEmail,
+              companyLogo: companyLogo,
+            ),
+          ),
+          const ApplyButton(),
+        ],
+      ),
     );
   }
 }
